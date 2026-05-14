35ea23653ffb7d3c23fbc9379b603420b1ea40c1
Artem Mahaz <ipz221_mao@student.ztu.edu.ua>
2026-05-14 20:35:45 +0200

    fixed axios

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import i18n from '../i18n';
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: `${import.meta.env.VITE_API_URL}/api`
 });
 
 api.interceptors.request.use((config) => {
